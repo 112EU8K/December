5440824f82d0ba66528c69754b80b56353f4ab19
112EU8K <90638908+112EU8K@users.noreply.github.com>
2025-01-21 15:33:32 +0300

    Update Logger.cpp

--- December/src/December/Logger.cpp
+++ December/src/December/Logger.cpp
@@ -2,14 +2,56 @@
 
 namespace December {
 
-    DECEMBER_API std::shared_ptr<spdlog::logger> Logger::EngineLogger;
-    DECEMBER_API std::shared_ptr<spdlog::logger> Logger::ClientLogger;
+    std::shared_ptr<spdlog::logger> Logger::EngineLogger;
+    std::shared_ptr<spdlog::logger> Logger::ClientLogger;
 
-    static void init() {
+    void Logger::init() {
         spdlog::set_pattern("%^[%T] %n: %v%$");
-        EngineLogger = spdlog::stdout_color_mt("DECEMBER");
-        EngineLogger->set_level(spdlog::level::trace);
-        ClientLogger = spdlog::stdout_color_mt("APP");
-        ClientLogger->set_level(spdlog::level::trace);
+        Logger::GetEngineLogger() = spdlog::stdout_color_mt("DECEMBER");
+        Logger::GetEngineLogger()->set_level(spdlog::level::trace);
+        Logger::GetClientLogger() = spdlog::stdout_color_mt("EVE");
+        Logger::GetClientLogger()->set_level(spdlog::level::trace);
+    }
+
+    // Engine log functions
+    void Logger::EngineTrace(const std::string& message) {
+        Logger::EngineLogger->trace(message);
+    }
+
+    void Logger::EngineInfo(const std::string& message) {
+        Logger::EngineLogger->info(message);
+    }
+
+    void Logger::EngineWarn(const std::string& message) {
+        Logger::EngineLogger->warn(message);
+    }
+
+    void Logger::EngineError(const std::string& message) {
+        Logger::EngineLogger->error(message);
+    }
+
+    void Logger::EngineFatal(const std::string& message) {
+        Logger::EngineLogger->critical(message);  // No "fatal"
+    }                                             // in spdlog;
+                                                  // use critical instead.
+    // Client log functions
+    void Logger::ClientTrace(const std::string& message) {
+        Logger::ClientLogger->trace(message);
+    }
+
+    void Logger::ClientInfo(const std::string& message) {
+        Logger::ClientLogger->info(message);
+    }
+
+    void Logger::ClientWarn(const std::string& message) {
+        Logger::ClientLogger->warn(message);
+    }
+
+    void Logger::ClientError(const std::string& message) {
+        Logger::ClientLogger->error(message);
+    }
+
+    void Logger::ClientFatal(const std::string& message) {
+        Logger::ClientLogger->critical(message);
     }
 }
