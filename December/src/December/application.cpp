743b7a138fd3f69904f0146069bf1664490309d6
112EU8K <90638908+112EU8K@users.noreply.github.com>
2025-01-23 04:48:11 +0300

    Update application.cpp

--- December/src/December/application.cpp
+++ December/src/December/application.cpp
@@ -1,8 +1,16 @@
 #include "application.h"
 
+#include "Events/ApplicationEvent.h"
+#include "Logger.h"
+
 namespace December
 {
     void Application::run() {
+
+        WindowResizeEvent e(1280, 720);
+        December::Logger::init();
+        December::Logger::EngineInfo(e.ToString());
+
         while (true);
     }
 }
