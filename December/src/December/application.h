8111c11b037feb67072b76a281e71d85cadfe55c
112EU8K <90638908+112EU8K@users.noreply.github.com>
2025-02-04 14:10:41 +0300

    Update application.h

--- December/src/December/application.h
+++ December/src/December/application.h
@@ -1,18 +1,23 @@
 #ifndef APPLICATION_H
 #define APPLICATION_H
+#include <Events/Event.h>
+#include <Window.h>
+#include <memory>
+#include <Core.h>
 
 namespace December
 {
-     class __declspec(dllexport) Application
+    class Application
     {
     public:
-        Application() = default;
-        virtual ~Application() = default;
+        Application();
+        virtual ~Application(); // Just declare it virtual, implementation goes in cpp
         void run();
-
+    private:
+        std::unique_ptr<Window> window;
+        bool running = true;
     };
 
     Application* CreateApplication();
 }
-
 #endif // APPLICATION_H
