755a58a3f35f7df56d8357f57bb2f933ad7c5e78
112EU8K <90638908+112EU8K@users.noreply.github.com>
2025-02-04 14:11:28 +0300

    Update application.cpp

--- December/src/December/application.cpp
+++ December/src/December/application.cpp
@@ -2,15 +2,33 @@
 
 #include "Events/ApplicationEvent.h"
 #include "Logger.h"
+#include "WindowsWindow.h"
 
 namespace December
 {
+    Application::Application()
+    {
+        Logger::init(); // Initialize logger first
+        window = std::unique_ptr<Window>(Window::Create());
+    }
+
+    Application::~Application()
+    {
+
+    };  // Define the destructor here
+
     void Application::run() {
+        while (running) {
+            window->onUpdate();
 
-        WindowResizeEvent e(1280, 720);
-        December::Logger::init();
-        December::Logger::EngineInfo(e.ToString());
-
-        while (true);
+            // Check if window is still open
+            if (auto* sfmlWindow = dynamic_cast<WindowsWindow*>(window.get()))
+            {
+                if (!sfmlWindow->GetNativeWindow()->isOpen())
+                {
+                    running = false;
+                }
+            }
+        }
     }
 }
