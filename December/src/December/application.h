6cfc3eca5883191f4ae61684d73f76e0c36c8914
112EU8K <90638908+112EU8K@users.noreply.github.com>
2025-02-04 22:27:14 +0300

    Update application.h

--- December/src/December/application.h
+++ December/src/December/application.h
@@ -1,6 +1,7 @@
 #ifndef APPLICATION_H
 #define APPLICATION_H
 #include <Events/Event.h>
+#include <Events/ApplicationEvent.h>
 #include <Window.h>
 #include <memory>
 #include <Core.h>
@@ -13,7 +14,9 @@ namespace December
         Application();
         virtual ~Application(); // Just declare it virtual, implementation goes in cpp
         void run();
+        void onEvent(Event &eve);
     private:
+        bool onWindowClose(WindowCloseEvent &e);
         std::unique_ptr<Window> window;
         bool running = true;
     };
