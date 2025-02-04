173380c6784a169549f8ebac60717b305a30db59
112EU8K <90638908+112EU8K@users.noreply.github.com>
2025-02-04 22:28:16 +0300

    Update WindowsWindow.cpp

--- December/Platform/Windows/WindowsWindow.cpp
+++ December/Platform/Windows/WindowsWindow.cpp
@@ -1,7 +1,9 @@
 #include "WindowsWindow.h"
+#include "Events/ApplicationEvent.h"
+#include "Events/KeyEvent.h"
+#include "Events/MouseEvent.h"
 
 namespace December {
-    static bool SFInitialized = false;
     Window* Window::Create(const December::WindowProps& props)
     {
         return new WindowsWindow(props);
@@ -34,25 +36,97 @@ namespace December {
 
         December::Logger::EngineInfo(ss.str());
 
-        if (!SFInitialized)
-        {
-            SFInitialized = true;
-        }
-
-        window = new sf::RenderWindow(sf::VideoMode(data.width, data.height), data.Title);
+        window = std::make_unique<sf::RenderWindow>(sf::VideoMode(data.width, data.height), data.Title);
         window->setActive(true);
         SetVSync(true);
     }
 
+    void WindowsWindow::pollEvents()
+    {
+        sf::Event event;
+        while (window->pollEvent(event))
+        {
+            switch (event.type)
+            {
+                case sf::Event::Closed:
+                {
+                    WindowCloseEvent e;
+                    data.EventCallBack(e);
+                    window->close();
+                    break;
+                }
+                case sf::Event::Resized:
+                {
+                    WindowResizeEvent e(event.size.width, event.size.height);
+                    data.width = event.size.width;
+                    data.height = event.size.height;
+                    data.EventCallBack(e);
+                    break;
+                }
+                case sf::Event::KeyPressed:
+                {
+                    KeyPressedEvent e(event.key.code, false);
+                    data.EventCallBack(e);
+                    break;
+                }
+                case sf::Event::KeyReleased:
+                {
+                    KeyReleasedEvent e(event.key.code);
+                    data.EventCallBack(e);
+                    break;
+                }
+                case sf::Event::TextEntered:
+                {
+                    if (event.text.unicode < 128) { // Ensure it's a valid ASCII character
+                        char character = static_cast<char>(event.text.unicode);
+                        KeyTypedEvent e(character);
+                        data.EventCallBack(e);
+                    }
+                    break;
+                }
+                case sf::Event::MouseButtonPressed:
+                {
+                    MouseButtonPressedEvent e(event.mouseButton.button);
+                    data.EventCallBack(e);
+                    break;
+                }
+                case sf::Event::MouseButtonReleased:
+                {
+                    MouseButtonReleasedEvent e(event.mouseButton.button);
+                    data.EventCallBack(e);
+                    break;
+                }
+                case sf::Event::MouseMoved:
+                {
+                    MouseMovedEvent e(static_cast<float>(event.mouseMove.x), static_cast<float>(event.mouseMove.y));
+                    data.EventCallBack(e);
+                    break;
+                }
+                case sf::Event::MouseWheelScrolled:
+                {
+                    MouseScrolledEvent e(event.mouseWheelScroll.delta, 0);
+                    data.EventCallBack(e);
+                    break;
+                }
+                default:
+                    break;
+            }
+        }
+    }
+
+
+
     void WindowsWindow::Shutdown()
     {
-        delete window;
+        window.reset();
     }
+
 
     void WindowsWindow::onUpdate()
     {
         while (window->pollEvent(event))
         {
+            pollEvents();
             if (event.type == sf::Event::Closed)
             {
                 window->close();
@@ -60,6 +134,7 @@ namespace December {
                 // You might want to add a way to communicate this to the main loop
             }
         }
+
 
         window->clear(); // Clear the window
         // Your rendering code would go here
