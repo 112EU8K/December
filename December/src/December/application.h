a9e92068b31f1d9615d1460d37198dbb59500898
112EU8K <90638908+112EU8K@users.noreply.github.com>
2025-02-05 20:31:01 +0300

    Update application.h

--- December/src/December/application.h
+++ December/src/December/application.h
@@ -1,10 +1,11 @@
 #ifndef APPLICATION_H
 #define APPLICATION_H
-#include <Events/Event.h>
-#include <Events/ApplicationEvent.h>
-#include <Window.h>
+#include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
+#include "Window.h"
+#include "LayerStack.h"
 #include <memory>
-#include <Core.h>
+#include "Core.h"
 
 namespace December
 {
@@ -15,10 +16,13 @@ namespace December
         virtual ~Application(); // Just declare it virtual, implementation goes in cpp
         void run();
         void onEvent(Event &eve);
+        void PushLayer(Layer* layer);
+        void PushOverlayer(Layer* overlay);
     private:
         bool onWindowClose(WindowCloseEvent &e);
         std::unique_ptr<Window> window;
         bool running = true;
+        LayerStack layer_stack;
     };
 
     Application* CreateApplication();
